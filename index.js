b4c86469267226a179675a0a3c1ee2be71c89391
Mark Mitchell <mmitchell88@hotmail.co.uk>
2019-05-09 14:30:31 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function takeANumber(line,name){
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(line){
+  if(line.length === 0){
+  return `There is nobody waiting to be served!`
+}
+}
